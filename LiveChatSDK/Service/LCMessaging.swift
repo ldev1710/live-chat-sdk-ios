560fa42503dae4c6c2e4a08e82c940498f386c98
Quốc Diễn <quoc.dien@mitek.vn>
2024-07-24 16:38:33 +0700

    log type

--- LiveChatSDK/Service/LCMessaging.swift
+++ LiveChatSDK/Service/LCMessaging.swift
@@ -21,7 +21,36 @@ open class LCMessaging: NSObject, UNUserNotificationCenterDelegate {
                               fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         LCLog.logI(message: "Framework received data message: \(data)")
         do {
-            var dataDict = data as! [String:Any]
+            var dataDict: [String:Any] = [:]
+            for (key, value) in data {
+                if let stringKey = key as? String {
+                    if let valueDict = value as? [String: Any] {
+                        dataDict[stringKey] = valueDict
+                        LCLog.logI(message: "valueDict: \(valueDict)")
+                    } else if let valueArray = value as? [Any] {
+                        dataDict[stringKey] = valueArray
+                        LCLog.logI(message: "valueNSString: \(valueArray)")
+                    } else if let valueString = value as? String {
+                        dataDict[stringKey] = valueString
+                        LCLog.logI(message: "valueString: \(valueString)")
+                    } else if let valueInt = value as? Int {
+                        dataDict[stringKey] = valueInt
+                        LCLog.logI(message: "valueInt: \(valueInt)")
+                    } else if let valueBool = value as? Bool {
+                        dataDict[stringKey] = valueBool
+                        LCLog.logI(message: "valueBool: \(valueBool)")
+                    } else if let valueDouble = value as? Double {
+                        dataDict[stringKey] = valueDouble
+                        LCLog.logI(message: "valueDouble: \(valueDouble)")
+                    } else if let valueNSString = value as? NSString{
+                        dataDict[stringKey] = valueNSString
+                        LCLog.logI(message: "valueNSString: \(valueNSString)")
+                    } else {
+                        // Handle other types if needed
+                        LCLog.logI(message: "Unsupported value type for key: \(stringKey)")
+                    }
+                }
+            }
             let contentRaw = dataDict["content"] as! [String:Any]
             let fromRaw = dataDict["sender"] as! [String: Any]
             let lcMessage = LCMessage(
