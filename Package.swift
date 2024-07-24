2d4fcfdf28914aea736883669ef243b724473d4d
Quốc Diễn <quoc.dien@mitek.vn>
2024-07-24 10:55:58 +0700

    add target firebase

--- Package.swift
+++ Package.swift
@@ -5,14 +5,13 @@ import PackageDescription
 let package = Package(
     name: "LiveChatSDK",
     products: [
-        .library(name: "LiveChatSDK", targets: ["LiveChatSDK"])
+        .library(name: "LiveChatSDK", targets: ["LiveChatSDK","Firebase"])
     ],
     dependencies: [
         .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.1.0")),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMinor(from: "10.29.0"))
-        
     ],
     targets: [
-        .target(name: "LiveChatSDK", dependencies: ["SocketIO", "Firebase"], path: "./LiveChatSDK")
+        .target(name: "LiveChatSDK", dependencies: ["SocketIO","Firebase"], path: "./LiveChatSDK"),
     ]
 )
