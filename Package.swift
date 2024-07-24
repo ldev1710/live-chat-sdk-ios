0619862ec4ab950ff7f191dd464695d533af628d
Quốc Diễn <quoc.dien@mitek.vn>
2024-07-24 11:03:01 +0700

    update package

--- Package.swift
+++ Package.swift
@@ -5,13 +5,20 @@ import PackageDescription
 let package = Package(
     name: "LiveChatSDK",
     products: [
-        .library(name: "LiveChatSDK", targets: ["LiveChatSDK","Firebase"])
+        .library(name: "LiveChatSDK", targets: ["LiveChatSDK"]),
     ],
     dependencies: [
         .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.1.0")),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMinor(from: "10.29.0"))
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.29.0")
     ],
     targets: [
-        .target(name: "LiveChatSDK", dependencies: ["SocketIO","Firebase"], path: "./LiveChatSDK"),
+        .target(
+            name: "LiveChatSDK",
+            dependencies: [
+                "SocketIO",
+                .product(name: "FirebaseMessaging", package: "Firebase"),
+            ],
+            path: "./LiveChatSDK"
+        ),
     ]
 )
